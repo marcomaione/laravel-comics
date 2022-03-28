b3c562ac2b3215d43fd221d5e47c7c6c8d09c079
marco maione <maionemarco14@gmail.com>
2022-03-28 15:45:43 +0200

    aggiornamneto

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,140 @@
+
+<footer>
+    <div class="container">
+        <div class="up">
+            <div class="items" v-for="(items, index) in digital" :key="index">>
+                <img :src="items.img" alt="">
+                <span>{{items.testo}}</span>
+            </div>
+            //non funziona non capisco il perchè
+            <div class="items">
+                <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div class="items">
+                <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div class="items">
+                <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div class="items">
+                <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+        </div>
+        <div class="midle">
+            <div class="centro">
+                <div class="midle-left">
+                    <div class="DC-1">
+                        <ul>
+                            <h4>DC COMICS</h4>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <h4>DC COMICS</h4>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="DC-2">
+                        <ul>
+                            <h4>DC</h4>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="DC-3">
+                        <ul>
+                            <h4>SITES</h4>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="midle-right">
+                    <img src="../assets/img/dc-logo-bg.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="under">
+           <div class="follow">
+                <div>
+                    <button>SIGN-UP NOW!</button>
+                </div>
+                <div>
+                    <a href="#">FOLLOW US</a>
+                    <img src="../assets/img/footer-facebook.png" alt="">
+                    <img src="../assets/img/footer-periscope.png" alt="">
+                    <img src="../assets/img/footer-pinterest.png" alt="">
+                    <img src="../assets/img/footer-twitter.png" alt="">
+                    <img src="../assets/img/footer-youtube.png" alt="">
+                </div>
+           </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,42 @@
+<header>
+    <div class="menu">
+        <img src="../assets/img/dc-logo.png" alt="">
+            <nav>
+                <ul>
+                        <li v-for="(link, index) in links" :key="index">
+                                <a href="#">{{link.text}}</a>
+                        </li>
+                        
+                        <li>
+                                <a href="#">COMICS</a>
+                        </li>
+                        <li>
+                                <a href="#">MOVIES</a>
+                        </li>
+                        <li>
+                                <a href="#">TV</a>
+                        </li>
+                        <li>
+                                <a href="#">GAMES</a>
+                        </li>
+                        <li>
+                                <a href="#">COLLECTIBLES</a>
+                        </li>
+                        <li>
+                                <a href="#">VIDEOS</a>
+                        </li>
+                        <li>
+                                <a href="#">FANS</a>
+                        </li>
+                        <li>
+                                <a href="#">NEWS</a>
+                        </li>
+                        <li>
+                                <a href="#">SHOP</a>
+                        </li>
+                    
+                </ul>
+            </nav>
+        
+    </div>
+</header>
