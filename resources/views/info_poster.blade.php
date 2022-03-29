8723373d6699fdbeda67db25ad38556d62dd8daf
marco maione <maionemarco14@gmail.com>
2022-03-29 18:00:50 +0200

    aggiornamento

--- resources/views/info_poster.blade.php
+++ resources/views/info_poster.blade.php
@@ -1,16 +1,19 @@
 <link rel="stylesheet" href="{{asset('css/app.css')}}">
 @include('partials.header')
 <main>
-     <div class="center">
-         <div class="poster">
-             <h2>{{$poster['title']}}</h2>
-             <img src="{{$poster['thumb']}}" alt="">
-             <h3>{{$poster['type']}}</h3>
-             <span>{{$poster['price']}}</span>
-             <span>{{$poster['sale_date']}}</span>
-             <p>{{$poster['description']}}</p>
-             <a href="/"><button>Gallery</button></a>
-         </div>
-     </div>
+    <div class="center">
+        <div class="poster">
+            <h2>{{$poster['title']}}</h2>
+            <img src="{{$poster['thumb']}}" alt="">
+            <h3>{{$poster['type']}}</h3>
+            <span>{{$poster['price']}}</span>
+            <span>{{$poster['sale_date']}}</span>
+        </div>
+        <div class="poster-l">
+            <h1>Descrizione</h1>
+            <p>{{$poster['description']}}</p>
+            <a href="/"><button>Gallery</button></a>
+        </div>
+    </div>
 </main>
 @include('partials.footer')
