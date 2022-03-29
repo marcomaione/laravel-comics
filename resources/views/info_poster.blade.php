76d3a5740b3593db77d974d658f6747c60daf032
marco maione <maionemarco14@gmail.com>
2022-03-29 21:37:21 +0200

    aggiornamento

--- resources/views/info_poster.blade.php
+++ resources/views/info_poster.blade.php
@@ -6,8 +6,14 @@
             <h2>{{$poster['title']}}</h2>
             <img src="{{$poster['thumb']}}" alt="">
             <h3>{{$poster['type']}}</h3>
-            <span>{{$poster['price']}}</span>
-            <span>{{$poster['sale_date']}}</span>
+        </div>
+        <div class="info">
+            <h1>Artisti</h1>
+            @foreach ($poster['artists'] as $artists)
+                <a href="#">{{$artists}}</a>
+            @endforeach
+            <span>Price-{{$poster['price']}}</span>
+            <span>Relase Date-</Relase-Date>{{$poster['sale_date']}}</span>
         </div>
         <div class="poster-l">
             <h1>Descrizione</h1>
